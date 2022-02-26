3a2851ec9b755ee7b6d0af0f18095a028fa2055f
Kuari <kuari@justmylife.cc>
2022-02-26 23:47:59 +0800

    feat:完成新增string的json编辑器支持

--- src/App.vue
+++ src/App.vue
@@ -8,4 +8,8 @@
   flex-direction: row;
   align-items: center;
 }
+
+body {
+  font: 14px/1.5 "Helvetica Neue",Helvetica,Arial,"Microsoft Yahei","Hiragino Sans GB","Heiti SC","WenQuanYi Micro Hei",sans-serif;
+}
 </style>
