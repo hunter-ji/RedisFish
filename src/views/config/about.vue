1d2c005955fd09b6fe016aec705f54d0c63046a1
Kuari <kuari@justmylife.cc>
2022-05-15 23:31:45 +0800

    fix:change version

--- src/views/config/about.vue
+++ src/views/config/about.vue
@@ -2,6 +2,6 @@
   <div class="about-container flex flex-col justify-center items-center py-4">
     <img src="@/assets/icon.png" alt="logo" width="120" height="120" class="rounded-full shadow hover:shadow-xl" />
     <div class="mt-4 text-2xl">RedFish</div>
-    <div class="mt-2 text-gray-400">Beta 2.0.0</div>
+    <div class="mt-2 text-gray-400">1.0.0</div>
   </div>
 </template>
